23c46293c1c484f578f7abfb2e53417d68f07263
ssontag55 <sontag.stephen@gmail.com>
2018-01-29 22:12:08 -0500

    JS commit

---
+++ widgets/ElevationProfile/nls/strings.js
@@ -0,0 +1,33 @@
+define({
+  root: ({
+    descriptionlabel: "Use the Measure tool to draw a line on the map that you want to see the elevation profile for.",
+    chartLabel: "Hover over or touch the Elevations Profile chart to display elevations and show location on map.",
+    clear: "Clear",
+    prepare: "Prepare Download...",
+    download: "Download",
+    measurelabel: "Measure",
+    resultslabel: "Profile Result",
+    profileinfo: "Profile Information",
+    display: {
+      elevationProfileTitle: "Elevation Profile",
+      hoverOver: "Hover over or touch the Elevations Profile chart to display elevations and show location on map."
+    },
+    chart: {
+      title: "Elevation Profile",
+      demResolution: "DEM Resolution",
+      elevationTitleTemplate: "Elevation in {0}",
+      distanceTitleTemplate: "Distance in {0}",
+      gainLossTemplate: "Elevation Min: {min} {unit}<br>Elevation Max: {max} {unit}<br>Elevation Start: {start} {unit}<br>Elevation End: {end} {unit}<br>Elevation Change: {gainloss} {unit}"
+    },
+    errors: {
+      InvalidConfiguration: "Invalid configuration.",
+      UnableToProcessResults: "Unable to process analysis results."
+    },
+    widgetversion: 'Elevation Profile Widget Version Info',
+    widgetverstr: 'Widget Version',
+    wabversionmsg: 'Widget is designed to run in Web AppBuilder version',
+    resultactions: "Result Menu",
+    _featureAction_exportToCSV: "Export profile data to CSV file",
+    flipProfile: "Flip elevation profile direction"
+  })
+});
